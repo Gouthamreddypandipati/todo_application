b67c40edd075be8dd1682ff5e995a11adba84de4
Gouthamreddypandipati <gouthampandipati@gmail.com>
2022-10-18 11:43:09 +0530

    initial commit

---
+++ tracker/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from .views import AddPost,Updat_task,Delete_task
+urlpatterns=[
+path('',views.base_page,name='base'),
+path('add_task/',AddPost.as_view(),name='add_t'),
+path('update_task/<int:pk>',Updat_task.as_view(),name='update_t'),
+path('delete_task/<int:pk>',Delete_task.as_view(),name='delete_t'),
+path('view',views.list_view,name='view'),
+
+]
